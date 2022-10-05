53ceb88d99bd50c669235a527b9af6dc6fbf4222
FranciscoSalanova <fsalanova.16@gmail.com>
2022-10-05 20:36:40 -0300

    Incorporacion de estilos y agregados de platillos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './styles/App.css';
 import Header from './components/header';
 import Dish from './components/dish';
+import './styles/App.css';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Dish />
+      <div className='menu'>
+        <Dish />
+        <Dish />
+        <Dish />
+      </div>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
--- src/components/dish.js
+++ src/components/dish.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 
+
+export class Ingredient extends Component {
+    render() {
+        return React.createElement('h4', {}, "Ingredientes:");
+    }
+}
+
 class Dish extends Component {
+    mensaje = 'Buen provecho!';
+    ingredientes = ['papa', 'tomate', 'aceite'];
+
     render() {
         return (
-            <div>
-                <p>TODO</p>
+            <div className='dish'>
+                <h1>Platillo</h1>
+                <Ingredient />
+                <ul>
+                    {
+                        this.ingredientes.map(ingrediente => {
+                            return <li>{ingrediente}</li>
+                        })
+                    }
+                </ul>
+                <p>{ this.mensaje }</p>
             </div>
         )
     }
